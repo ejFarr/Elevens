c12060d4d9c77c83ff9403cb2aa93e65533879f6
Evan Farr <roboproductions200@gmail.com>
2020-03-29 20:16:55 -0600

    finish a1 exercises

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,14 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card = new Card("king", "spades", 13);
+		Card card2 = new Card("two", "clubs", 2);
+		Card card3 = new Card("two", "clubs", 2);
+
+		System.out.println(card.rank());
+		System.out.println(card.suit());
+		System.out.println(card.pointValue());
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.toString());
 	}
 }
